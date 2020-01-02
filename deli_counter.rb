e63b3d3739402958b214fefa2b5d8739a6fc31a4
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-02 23:23:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,13 @@
 katz_deli = []
 
 def line(current_line) 
-  #Checks to see if empty
+  #Checks to see if empty: "The line is currently empty."
   #If not empty returns in format "The line is currently: 1. Grace 2. Kent"
 end
 
 def take_a_number(current_line, name)
-  
+  #puts the persons name and their positon can use push
+  #form: 
 end
 
 def now_serving 
